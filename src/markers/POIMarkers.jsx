e8f872b1f9368d22396d95f2bff09d22ce055600
Aidan Yu <aidanxiaomoyu@gmail.com>
2026-03-11 16:09:58 -0700

    added some poi markers

--- src/markers/POIMarkers.jsx
+++ src/markers/POIMarkers.jsx
@@ -6,6 +6,18 @@ function generatePOILink(name) {
 
 export const poiMarkers = [
   {
+    coords: [10828, 11373],
+    popUp: (
+      <PopupContent 
+        name="Altar of Insight"
+        link={ generatePOILink("Altar of Insight") } 
+        subtext="The Altar of Insight was an ancient, stone ziggurat by the Tinaxi Pools that contained unique Veruhktian technology and served as an important religious site."
+        imageURL="https://wiki.drehmal.cyou/assets/img/poi/altar_of_insight.png"
+        coords="(X: 4116, Y: 122, Z: -1547)"
+      />
+    )
+  },
+  {
     coords: [11274, 8754],
     popUp: (
       <PopupContent 
@@ -16,7 +28,139 @@ export const poiMarkers = [
         coords="(X: 1510, Y: 144, Z: -2012)"
       />
     )
-  }, 
+  },
+  {
+    coords: [7636, 7016.5],
+    popUp: (
+      <PopupContent 
+        name="Av'Sal"
+        link={ generatePOILink("Av'Sal") } 
+        subtext="Av'Sal is the ruined capital city of the Avsohmic Empire, located in southwestern Dreh'Noha near the northern edge of Palisades Heath. Av'Sal is among the largest ruins in the map, if not the largest."
+        imageURL="https://wiki.drehmal.cyou/assets/img/poi/avsal.png"
+        coords="(X: -218, Y: 105, Z: 1645)"
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
   {
     coords: [7763, 3956.5],
     popUp: (
@@ -28,5 +172,18 @@ export const poiMarkers = [
         coords="(X: -3276, Y: 157, Z: 1500)"
       />
     )
-  }, 
+  },
+  {
+    coords: [0, 0],
+    popUp: (
+      <PopupContent 
+        name=""
+        link={ generatePOILink("") } 
+        subtext=""
+        imageURL=""
+        coords=""
+      />
+    )
+  },
+
 ];
